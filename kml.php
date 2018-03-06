6bb72ca7feca04976c866e306f2e08ed95f8190e
Dhiraj Prakash <dhiraj@brazhuman.com>
2018-03-06 00:59:37 -0300

    kml file name changed

---
+++ kml.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>KML Layers</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: {lat: -22.7434969998076, lng: -47.331802}
+        });
+
+        var ctaLayer = new google.maps.KmlLayer({
+          url: 'https://raw.githubusercontent.com/dhirajprakash/googlemaps/master/americana.kml',
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkeZvStmcd4GC5HmpJT7mkEN1KFPQ7bPc&libraries=visualization&callback=initMap">
+    </script>
+  </body>
+</html>
